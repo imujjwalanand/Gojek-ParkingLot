7be57260fd722dfeefa2e82fee72e2f6a532183c
APTTUS\uanand <uanand@apttus.com>
2019-06-16 02:48:06 +0530

    Added Constants class to store hardcoded strings for I/O

---
+++ src/parking_lot/parking_lot/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parking_lot {
+    public class Constants {
+        public static string CREATE_PARKING_LOT = "Created a parking lot with {0} slots";
+        public static string CREATE_PARKING_LOT_COMMAND = "create_parking_lot";
+        public static string PARK_COMMAND = "park";
+        public static string LEAVE_COMMAND = "leave";
+        public static string STATUS_COMMAND = "status";
+        public static string REG_FOR_COLOUR_COMMAND = "registration_numbers_for_cars_with_colour";
+        public static string SLOT_FOR_COLOUR_COMMAND = "slot_numbers_for_cars_with_colour";
+        public static string SLOT_FOR_REG_COMMAND = "slot_number_for_registration_number";
+    }
+}
--- src/parking_lot/parking_lot/ParkingLot.cs
+++ src/parking_lot/parking_lot/ParkingLot.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace parking_lot {
     public class ParkingLot {
+        static Dictionary<string, string> _regToColourMap;
+        static Dictionary<int, string> _slotToRegMap;
+        static Dictionary<string, bool> _commands;
+        static int _lotSize;
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Initialize();
+            _regToColourMap = new Dictionary<string, string>();
+            _slotToRegMap = new Dictionary<int, string>();
+        }
+
+
+
+        public static void Initialize() {
+            _commands = new Dictionary<string, bool>();
+            _commands.Add(Constants.CREATE_PARKING_LOT_COMMAND, true);
+            _commands.Add(Constants.PARK_COMMAND, true);
+            _commands.Add(Constants.LEAVE_COMMAND, true);
+            _commands.Add(Constants.STATUS_COMMAND, true);
+            _commands.Add(Constants.REG_FOR_COLOUR_COMMAND, true);
+            _commands.Add(Constants.SLOT_FOR_COLOUR_COMMAND, true);
+            _commands.Add(Constants.SLOT_FOR_REG_COMMAND, true);
         }
     }
 }
