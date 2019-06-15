57a88b6892bc867fdaf5fd3193683bcd0a2ccffe
APTTUS\uanand <uanand@apttus.com>
2019-06-16 00:56:35 +0530

    Initialised empty git repo, initialised parking_lot ConsoleApp solution, added .gitignore file to ignore .vs folder

---
+++ src/parking_lot/parking_lot/ParkingLot.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace parking_lot {
+    public class ParkingLot {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
